8ca9d7c4b9747567c27cb3be8c454d039b2ec348
gineres <giselle.neres.contact@gmail.com>
2023-11-08 00:07:04 -0300

    Mapa com cores a depender da altura

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode {NoiseMap, ColorMap};
+    
+    public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -13,14 +16,42 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public int seed;
     public Vector2 offset;
 
     public void GenerateMap(){
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth,mapHeight,seed,noiseScale, octaves, persistence, lacunarity,offset);
 
+        Color[] colorMap = new Color[mapHeight*mapWidth];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x,y];
+                //Colorindo
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay> ();
-        display.DrawNoiseMap(noiseMap);
+
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColorMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+        
     }
 
     void OnValidate() // chamado quando muda uma variave l no inspetor
@@ -41,4 +72,12 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }
     }
+}
+
+// Para aparecer no inspetor bonitinho
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color color;
 }
