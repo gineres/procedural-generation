a282b7d6cca8745d9ad107c045b54c5fbefb4960
gineres <giselle.neres.contact@gmail.com>
2023-11-08 12:01:26 -0300

    Triângulos criados

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -7,12 +7,27 @@ public static class MeshGenerator
     public static void GenerateTerrainMesh(float[,] heightMap){
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
+        float topLeftX = (width - 1)/2f;
+        float topLeftZ = (height - 2)/2f;
+
+        MeshData meshData = new MeshData(width, height);
+        int vertexIndex = 0;
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x,y], topLeftZ - y);
+
+                // Para ignorar os vertices da borda direita e borda abaixo, subtrai -1
+                if (x < width - 1 && y < height - 1)
+                {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + width + 1, vertexIndex + width); // Triangulo 1 do quadrado
+                    meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1); // Triangulo 2 do quadrado
+                }
+
+
+                vertexIndex++;
             }
         }
     }
