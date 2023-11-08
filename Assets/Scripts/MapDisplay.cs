d3b94014d9e6957232b8b88b9f2602e2c7289763
gineres <giselle.neres.contact@gmail.com>
2023-11-08 13:07:44 -0300

    Gerando a mesh do plano

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRender; // Vai colocar o plano aqui, eh a superficie de onde vai ficar a textura
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
     public void DrawTexture(Texture2D texture){
         textureRender.sharedMaterial.mainTexture = texture;
         textureRender.transform.localScale = new Vector3 (texture.width, 1, texture.height); // Altera a escala do plano!! Kool!
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture){
+        meshFilter.sharedMesh = meshData.CreateMesh(); // Shared para o caso de gerar a mesh fora do modo gameplay
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class MeshGenerator
 {
-    public static void GenerateTerrainMesh(float[,] heightMap){
+    public static MeshData GenerateTerrainMesh(float[,] heightMap){
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width - 1)/2f;
@@ -18,6 +18,7 @@ public static class MeshGenerator
             for (int x = 0; x < width; x++)
             {
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightMap[x,y], topLeftZ - y);
+                meshData.uvs[vertexIndex] = new Vector2(x/(float)width, y/(float)height);
 
                 // Para ignorar os vertices da borda direita e borda abaixo, subtrai -1
                 if (x < width - 1 && y < height - 1)
@@ -26,10 +27,11 @@ public static class MeshGenerator
                     meshData.AddTriangle(vertexIndex + width + 1, vertexIndex, vertexIndex + 1); // Triangulo 2 do quadrado
                 }
 
-
                 vertexIndex++;
             }
         }
+        
+        return meshData;
     }
 }
 
@@ -39,9 +41,11 @@ public class MeshData
     public int[] triangles;
 
     int triangleIndex;
+    public Vector2[] uvs;
 
     public MeshData(int meshWidth, int meshHeight){
         vertices = new Vector3[meshWidth * meshHeight];
+        uvs = new Vector2[meshHeight * meshWidth];
         triangles = new int[(meshWidth-1) * (meshHeight-1) * 6]; // Triangulos sao a combinacao de triangulos possiveis dado os pontinhos dos vertices
         // Esse calculo faz essa operacao para saber quantos triangulos sao formados dado um numero de vertices
     }
@@ -51,5 +55,14 @@ public class MeshData
         triangles[triangleIndex + 1] = b;
         triangles[triangleIndex + 2] = c;
         triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh() {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        return mesh;
     }
 }
